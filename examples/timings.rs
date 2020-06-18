3f8a19e547f19af8e88c24e21df6524877159883
frederic wagner <frederic.wagner@imag.fr>
2020-06-18 11:34:35 +0200

    better message for overheads evaluation

--- examples/timings.rs
+++ examples/timings.rs
@@ -11,6 +11,8 @@ fn fibo(n: u32) -> u32 {
     }
 }
 
+const REPETITIONS: usize = 1_000_000;
+
 fn main() {
     let pool = ThreadPoolBuilder::new()
         .num_threads(4)
@@ -26,8 +28,19 @@ fn main() {
         });
         (t, d.duration)
     })
-    .take(10_000)
+    .take(REPETITIONS)
     .collect();
-    eprintln!("it took {}", t.iter().map(|(t, _)| t).sum::<u64>());
-    eprintln!("it took {}", t.iter().map(|(_, d)| d).sum::<u64>());
+
+    let inner_time = t.iter().map(|(t, _)| t).sum::<u64>();
+    let outer_time = t.iter().map(|(_, d)| d).sum::<u64>();
+    let overhead = (outer_time - inner_time) / REPETITIONS as u64;
+    println!(
+        "we estimate a logging overhead of approximately {} ns per logged event",
+        overhead
+    );
+    println!(
+        "if you want to keep logging overheads below 1% we advise you to log
+        only tasks with a duration larger than {} ns",
+        overhead * 100
+    );
 }
