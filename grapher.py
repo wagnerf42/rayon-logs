020401e5d0f6ab7d816503e0ae4bea2b2cb941ac
frederic wagner <frederic.wagner@imag.fr>
2020-09-11 18:10:10 +0200

    cleaned graph code a bit

--- grapher.py
+++ grapher.py
@@ -9,7 +9,12 @@ from more_itertools import windowed, roundrobin
 
 COLORS = ["red", "green", "blue"]
 
+
 class Task:
+    """
+    One Task is not a rayon task but one box to animate
+    """
+
     def __init__(self, start_time, end_time, thread):
         self.start_time = start_time
         self.end_time = end_time
@@ -17,18 +22,39 @@ class Task:
         self.width = end_time - start_time
         self.height = 1
         self.position = [0, 0]
+
     def dimensions(self):
+        """
+        return width and height needed for display
+        """
         return [self.width, self.height]
+
     def compute_positions(self):
+        """
+        this is an empty function to stop recursion from SubGraph
+        """
         pass
+
     def svg(self, scales):
         """
         print svg code for this task's display
         """
         color = COLORS[self.thread % len(COLORS)]
-        print("<rect width='{}' height='{}' x='{}' y='{}' style='fill:{}'/>".format(self.width*scales[0], scales[1], self.position[0] * scales[0], self.position[1] * scales[1], color))
+        print("<rect width='{}' height='{}' \
+x='{}' y='{}' style='fill:{}'\
+/>".format(self.width*scales[0], scales[1]/2,
+           self.position[0] * scales[0],
+           # let's skip some height to leave space for edges
+           (self.position[1]+0.25) * scales[1], color))
+
 
 class SubGraph:
+    """
+    one node in a tree constituing the graph.
+    contains either a sequence of subgraphs
+    or parallel subgraphs
+    """
+
     def __init__(self, content, is_parallel):
         self.content = content
         self.is_parallel = is_parallel
@@ -40,6 +66,9 @@ class SubGraph:
         self.position = [0, 0]
 
     def dimensions(self):
+        """
+        return width and height
+        """
         return self.size
 
     def svg(self, scales):
@@ -70,33 +99,78 @@ class SubGraph:
 
 
 def build_graph(root, children, spans):
-    if children[root]:
-        subgraphs = (build_graph(c, children, spans) for c in children[root])
-        if spans[root]["tag"] == "join":
-            content = subgraphs
-            is_parallel = True
-        else:
-            # we need to loop on all in between periods
-            times = ((spans[c]["start"], spans[c]["end"])
-                      for c in children[root])
-            all_times = chain([(None, spans[root]["start"])],
-                              times,
-                              [(spans[root]["end"], None)],
-                        )
-            consecutives = windowed(all_times, 2)
-            intervals = ((c[0][1], c[1][0]) for c in consecutives)
-            thread = spans[root]["thread"]
-            tasks = (Task(start, end, thread) for start, end in intervals)
-            content = roundrobin(tasks, subgraphs)
-            is_parallel = False
-        return SubGraph(list(content), is_parallel)
+    """
+    build a tree corresponding to the task graph rooted at given root.
+    positions are not computed as they need a second pass
+    """
+    subgraphs = (build_graph(c, children, spans) for c in children[root])
+    if spans[root]["tag"] == "join":
+        # in between periods are hidden. they should appear in idle gantt
+        content = subgraphs
+        is_parallel = True
     else:
-        # if we have no children we are a final task
-        return Task(
-                    spans[root]["start"],
-                    spans[root]["end"],
-                    spans[root]["thread"],
-                )
+        # we need to loop on all "in between" periods
+        times = ((spans[c]["start"], spans[c]["end"])
+                 for c in children[root])
+        all_times = chain([(None, spans[root]["start"])],
+                          times,
+                          [(spans[root]["end"], None)])
+        consecutives = windowed(all_times, 2)
+        intervals = ((c[0][1], c[1][0]) for c in consecutives)
+        thread = spans[root]["thread"]
+        tasks = (Task(start, end, thread) for start, end in intervals)
+        content = roundrobin(tasks, subgraphs)
+        is_parallel = False
+    return SubGraph(list(content), is_parallel)
+
+
+def load_spans(filename):
+    """
+    load json file containing spans and indexed them by id
+    """
+    with open(filename) as json_file:
+        spans = json.load(json_file)
+
+    indexed_spans = dict()
+    for span in spans:
+        identifier = span["ID"]
+        assert identifier != 0  # 0 is reserved for no parent
+        assert identifier not in indexed_spans  # all are unique
+        indexed_spans[identifier] = span
+    return indexed_spans
+
+
+class Graph:
+    """
+    fork-join task graph as displayed in rayon-logs
+    """
+
+    def __init__(self, spans):
+        # compute children and root spans
+        children = defaultdict(list)
+        roots = []
+        for span_id, span in spans.items():
+            if span["parent"] != 0:
+                children[span["parent"]].append(span_id)
+            else:
+                roots.append(span_id)
+
+        # for now, we only work with one root
+        assert len(roots) == 1
+
+        self.root = build_graph(roots[0], children, spans)
+        self.root.compute_positions()
+
+    def svg(self):
+        """
+        print svg code
+        """
+        print('<svg version="1.1" viewBox="0 0 1920 1080" \
+    xmlns="http://www.w3.org/2000/svg">')
+        size = self.root.dimensions()
+        scales = (1920/size[0], 1080/size[1])
+        self.root.svg(scales)
+        print('</svg>')
 
 
 def main():
@@ -106,27 +180,10 @@ def main():
     if len(sys.argv) != 2:
         print("we need a json log filename")
         sys.exit(1)
-    with open(sys.argv[1]) as json_file:
-        spans = json.load(json_file)
 
-    # TODO: we should check there is no 0 and no duplicates
-    spans = dict((s["ID"], s) for s in spans)
-
-    children = defaultdict(list)
-    roots = []
-    for span_id, span in spans.items():
-        if span["parent"] != 0:
-            children[span["parent"]].append(span_id)
-        else:
-            roots.append(span_id)
-    assert len(roots) == 1
-    graph = build_graph(roots[0], children, spans)
-    graph.compute_positions()
-    print('<svg version="1.1" viewBox="0 0 1920 1080" xmlns="http://www.w3.org/2000/svg">')
-    size = graph.dimensions()
-    scales = (1920/size[0], 1080/size[1])
-    graph.svg(scales)
-    print('</svg>')
+    spans = load_spans(sys.argv[1])
+    graph = Graph(spans)
+    graph.svg()
 
 
 main()
